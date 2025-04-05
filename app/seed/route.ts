f8d12b73389f8b6b6ff434c582e17a76f375feb2
glunetto <gas-o@hotmail.it>
2025-04-05 17:08:33 +0200

    Chapter 6

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -5,15 +5,16 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
-    );
-  `;
+  // await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await sql`
+  //   CREATE TABLE IF NOT EXISTS users (
+  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  //     name VARCHAR(255) NOT NULL,
+  //     email TEXT NOT NULL UNIQUE,
+  //     password TEXT NOT NULL
+  //   );
+  // `;
+  await sql`TRUNCATE TABLE users;`;
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
@@ -30,17 +31,17 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-  await sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      customer_id UUID NOT NULL,
-      amount INT NOT NULL,
-      status VARCHAR(255) NOT NULL,
-      date DATE NOT NULL
-    );
-  `;
+  // await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await sql`
+  //   CREATE TABLE IF NOT EXISTS invoices (
+  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  //     customer_id UUID NOT NULL,
+  //     amount INT NOT NULL,
+  //     status VARCHAR(255) NOT NULL,
+  //     date DATE NOT NULL
+  //   );
+  // `;
+  await sql`TRUNCATE TABLE invoices;`;
 
   const insertedInvoices = await Promise.all(
     invoices.map(
@@ -56,16 +57,16 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-  await sql`
-    CREATE TABLE IF NOT EXISTS customers (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      image_url VARCHAR(255) NOT NULL
-    );
-  `;
+  // await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await sql`
+  //   CREATE TABLE IF NOT EXISTS customers (
+  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  //     name VARCHAR(255) NOT NULL,
+  //     email VARCHAR(255) NOT NULL,
+  //     image_url VARCHAR(255) NOT NULL
+  //   );
+  // `;
+  await sql`TRUNCATE TABLE customers;`;
 
   const insertedCustomers = await Promise.all(
     customers.map(
@@ -81,12 +82,13 @@ async function seedCustomers() {
 }
 
 async function seedRevenue() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS revenue (
-      month VARCHAR(4) NOT NULL UNIQUE,
-      revenue INT NOT NULL
-    );
-  `;
+  // await sql`
+  //   CREATE TABLE IF NOT EXISTS revenue (
+  //     month VARCHAR(4) NOT NULL UNIQUE,
+  //     revenue INT NOT NULL
+  //   );
+  // `;
+  await sql`TRUNCATE TABLE revenue;`;
 
   const insertedRevenue = await Promise.all(
     revenue.map(
@@ -103,11 +105,11 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
+    await sql.begin((sql) => [
+      // seedCustomers(),
+      // seedInvoices(),
+      // seedRevenue(),
+      // seedUsers(),
     ]);
 
     return Response.json({ message: 'Database seeded successfully' });
